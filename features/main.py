1872d352677a7041c1f9d80d897c01c2067fb133
Yehor_9767624362362738q9!@ <yk.cm.sb@gmail.com>
2025-11-12 14:21:32 +0200

    Add files via upload

---
+++ features/main.py
@@ -0,0 +1,33 @@
+"""Main module for the assistant bot."""
+
+from handlers import commands, parse_input, handle_exit
+from address_book import AddressBook
+
+def main():
+    """Main function to run the assistant bot."""
+    book = AddressBook.load()
+
+    print("Welcome to the assistant bot!")
+    while True:
+        try:
+            user_input = input("\nEnter a command: ")
+        except (KeyboardInterrupt, EOFError):
+            print()  # For a Ctrl+C newline on exit
+            handle_exit(book)
+
+        command, args = parse_input(user_input) # type: ignore
+
+        if command is None:
+            print("- No command entered.")
+            continue
+
+        func = commands.get(command)
+        if func is None:
+            print("- Invalid command.")
+            continue
+
+        print('-', func(book, *args))
+
+
+if __name__ == "__main__":
+    main()
