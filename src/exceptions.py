9ab5e22218beabbef4113a65df11d19545b5ea0a
Yehor_9767624362362738q9!@ <yk.cm.sb@gmail.com>
2025-11-15 13:49:17 +0200

    Delete src directory

--- src/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""Custom exceptions for the assistant bot."""
-
-class ValidationError(Exception):
-    """Custom exception for validation errors."""
-
-class NoteError(Exception):
-    """Custom exception for note errors."""
