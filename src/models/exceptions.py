3622b966d59003105b4f40e3d2c8093d0bdeaa1c
Yehor_9767624362362738q9!@ <yk.cm.sb@gmail.com>
2025-11-16 11:47:03 +0200

    Add files via upload

---
+++ src/models/exceptions.py
@@ -0,0 +1,7 @@
+"""Custom exceptions for the assistant bot."""
+
+class ValidationError(Exception):
+    """Custom exception for validation errors."""
+
+class NoteError(Exception):
+    """Custom exception for note errors."""
